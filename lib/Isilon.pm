df6635b8e755c2897258b13ada1ba6d0ab92b888
Steven Kreuzer <steven@stevenbsd.hudson-trading.com>
2010-08-17 17:27:40 -0400

    Create sub modules

---
+++ lib/Isilon.pm
@@ -0,0 +1,14 @@
+package Isilon;
+
+use version; $VERSION = qv('0.1');
+
+use strict;
+use warnings;
+
+use Isilon::Cluster;
+use Isilon::Node;
+use Isilon::SyncIQ;
+use Isilon::SnapshotIQ;
+use Isilon::SmartQuotas;
+
+1;
